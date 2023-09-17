d1d2799ceed8e564fe5562c86534b82ee051dd63
parsoli83 <mr.parsoli@gmail.com>
2023-09-17 18:57:42 +0330

    finished lec_0

---
+++ week_0/lec_0.py
@@ -0,0 +1,27 @@
+"""
+binary:
+when 2 digits :)
+
+bit = binary digit
+
+so much bullshit :)
+
+about bits:
+
+00 0
+01 1
+10 2
+11 3
+
+#keep in mind it starts from 0 to 2**n-1
+#thats why colors are from 0 to 255 in pillow :)
+
+*** ASCII ***
+
+a standard way of representing characters with numbers :)
+
+*** binary tree search ***
+
+yeah divide in half and ask which way :)
+"""
+ 
